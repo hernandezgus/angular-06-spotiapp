18e95adf00fa205785cfb0c226bafd663657ff39
hernandezgus <hernandez.gustavo@outlook.com>
2020-03-18 00:51:37 -0300

    Testing spotify service instantiation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpotifyService } from '../spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor( private spotifyService: SpotifyService ) { }
 
 }
