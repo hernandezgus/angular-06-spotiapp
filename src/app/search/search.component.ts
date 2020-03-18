9f7cb9ca9809440d15009b7802de94ae2177c7fa
hernandezgus <hernandez.gustavo@outlook.com>
2020-03-18 01:20:33 -0300

    Adding code for search artist using spotify API

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { SpotifyService } from '../spotify.service';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  artistas: any[] = [];
+  constructor(private spotifyService: SpotifyService ) { }
 
-  ngOnInit() {
+  searchArtist(termino: string)
+  {
+    console.log(termino);
+    this.spotifyService.searchArtist(termino)
+      .subscribe( (data: any) => {
+        console.log(data.artists.items);
+        this.artistas = data.artists.items;
+      });
   }
-
 }
--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -14,4 +14,11 @@ export class SpotifyService {
     });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
   }
+
+  searchArtist(termino: string) {
+    const headers = new HttpHeaders ({
+      'Authorization': 'Bearer BQAkW--4xIJb3pXW9yV6zBKjjRND_EJMEqve9PE0edoM33q1Ss1CfG3B7lXEELgnDvPrWTd6cMaR7ikMe8I'
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&market=UY&limit=15`, { headers });
+  }
 }
